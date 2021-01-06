6e64f15dc75e5644d346a3fea38e2f7400c73bcd
hyeonjung0615 <hjn7200@naver.com>
2021-01-06 11:56:47 +0900

    Create Constant.java

---
+++ chapter2/binary/Constant.java
@@ -0,0 +1,17 @@
+package binary;
+
+public class Constant {
+	
+	public static void main(String[] args)
+	{
+		final int MAX_NUM = 100;
+		final float PI = 3.14f; // final double PI = 3.14;
+		
+		final int STDUENT_NUM = 30;
+		
+		int num = 0;
+		if ( num == STDUENT_NUM ) {}
+		
+		System.out.println(STDUENT_NUM);
+	}
+}
