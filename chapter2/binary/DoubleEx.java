6d49db1a5b712259c6f8a76d7d6fd7df00e0371e
hyeonjung0615 <hjn7200@naver.com>
2021-01-06 11:57:08 +0900

    Create DoubleEx.java

---
+++ chapter2/binary/DoubleEx.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class DoubleEx {
+	
+	int abc;
+	
+	public static void main(String[] args)
+	{
+		double dNum = 3.14; //var dNum
+		float fNum = 3.14F; 
+		System.out.println(dNum);
+		
+		var num = 10;
+		System.out.println(num);
+		// NO! num = 3.14;
+	}
+
+}
